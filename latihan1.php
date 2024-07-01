7c5acb888a10538b4f7e75fdd84c65d455cf3b1a
dionkurniawan <yogitricahya21>
2024-07-01 12:01:18 +0700

    masuk ngga yaa

---
+++ latihan1.php
@@ -0,0 +1,12 @@
+<?php
+echo "Hello World"
+// perintah untuk tambah/ add
+// 1. git add . atau git add nama_folder/file
+//perintah untuk commit atau komentar
+// 2. git commit -m "komentarnya apa"
+// perintah untuk upload atau push
+// 3- git push -u origin main
+// push adalah perintah
+// origin adalah nama remote
+// "main" adalah branch
+?>
